1fe7eef6d7560b3489e734f082827abebc5b5e9a
lundbaek <leif.lundbaek@outlook.de>
2016-07-14 12:12:44 +0100

    Changed middle name for Darwin

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -9,7 +9,7 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
         if (query.toLowerCase().contains("darwin")) {
-            return "Charles Robert Darwin, FRS FRGS FLS FZS (12 February 1809 – 19 April 1882)"+
+            return "Charles Darwin, FRS FRGS FLS FZS (12 February 1809 – 19 April 1882)"+
                     " was an English naturalist and geologist, best known for his contributions"+
                     " to the science of evolution.";
         }
